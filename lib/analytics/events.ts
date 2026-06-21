cba0b35842795b3cbbc163ea401d77e34cfd15ac
Brok <brok110@gmail.com>
2026-06-21 15:40:59 -0700

    add analytics events for recommendation viewed/engaged and restock click

---
+++ lib/analytics/events.ts
@@ -0,0 +1,13 @@
+export const EVENTS = {
+  APP_OPENED: 'app_opened',
+
+  INVENTORY_CREATED: 'inventory_created',
+  BOTTLE_ADDED: 'bottle_added',
+
+  RECOMMENDATION_GENERATED: 'recommendation_generated',
+  RECOMMENDATION_VIEWED: 'recommendation_viewed',
+  RECOMMENDATION_ENGAGED: 'recommendation_engaged',
+
+  SMART_RESTOCK_VIEWED: 'smart_restock_viewed',
+  RESTOCK_ITEM_CLICKED: 'restock_item_clicked',
+} as const;
